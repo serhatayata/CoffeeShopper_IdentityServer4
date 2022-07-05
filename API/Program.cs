bdda09bb93e9786db93d907f3422d1b28f5fb5c3
Serhat Ayata <srhtayata@gmail.com>
2022-07-05 17:46:22 +0300

    IdentityServer added - JWT Token is working now, but still goes on

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 #region Controller
 builder.Services.AddControllers();
 #endregion
+#region Auth
+builder.Services.AddAuthentication("Bearer")
+    .AddIdentityServerAuthentication("Bearer", options =>
+    {
+        //Server address
+        options.Authority = "https://localhost:5443";
+        //Server Config.cs'te belirtilen ApiResource
+        options.ApiName = "CoffeeAPI";
+    });
+#endregion
 #region DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 #endregion
@@ -18,6 +28,8 @@ builder.Services.AddScoped<ICoffeeShopService, CoffeeShopService>();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
---
+++ Client/Services/TokenService.cs
@@ -0,0 +1,43 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+
+namespace Client.Services
+{
+    public class TokenService : ITokenService
+    {
+        public readonly IOptions<IdentityServerSettings> _identityServerSettings;
+        public readonly DiscoveryDocumentResponse discoveryDocument;
+        private readonly HttpClient httpClient;
+
+        public TokenService(IOptions<IdentityServerSettings> identityServerSettings)
+        {
+            _identityServerSettings = identityServerSettings;
+            httpClient = new HttpClient();
+            discoveryDocument = httpClient.GetDiscoveryDocumentAsync(_identityServerSettings.Value.DiscoveryUrl).Result;
+
+            if (discoveryDocument.IsError)
+            {
+                throw new Exception("Unable to get discovery document", discoveryDocument.Exception);
+            }
+        }
+
+        public async Task<TokenResponse> GetToken(string scope)
+        {
+            var tokenResponse = await httpClient.
+                RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+                {
+                    Address = discoveryDocument.TokenEndpoint,
+                    ClientId = _identityServerSettings.Value.ClientName,
+                    ClientSecret = _identityServerSettings.Value.ClientPassword,
+                    Scope = scope
+                });
+
+            if (tokenResponse.IsError)
+            {
+                throw new Exception("Unable to get token", tokenResponse.Exception);
+            }
+
+            return tokenResponse;
+        }
+    }
+}
